8b84977f750d3448e239d9cd9c4690318ddacb55
Rovshan SH. Guliyev <rovshanshg@code.local>
2021-12-07 12:23:54 +0400

    PowerOfNumbers

---
+++ Static_Extention/Static_Extention/Extentions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Extention
+{
+    public static class  Extentions
+    {
+        public static int PowerOfNumbers(this int n, int m)
+        {
+            double power = Math.Pow(n, m);
+            return Convert.ToInt32(power);
+        }
+    }
+}
---
+++ Static_Extention/Static_Extention/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Static_Extention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = 5;
+            int result = power.PowerOfNumbers(10);
+            Console.WriteLine(result);
+
+        }
+    }
+}
